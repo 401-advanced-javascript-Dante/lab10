65859ad4159d0407a5d8b3ce44d7c5a0a89c9464
dantemessy <thepunishar1@gmail.com>
2020-02-02 21:28:01 +0200

    base server for next labs

---
+++ __test__/APIs.test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// const {server} = require('../lib/server.js');
+// for mock testing ...
+// const supergoose = require('@code-fellows/supergoose');
+// const mockRequest = supergoose(server) ;
+
+
+// ready to test the routes 
+describe('Test' , () => {
+
+  it('' , () => {
+
+  });
+  it('' , () => {
+        
+  });
+
+  it('' , () => {
+        
+  });
+
+
+});
