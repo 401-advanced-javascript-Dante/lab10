1ba701ed381ad62f66830f3f22366bb243d5d530
dantemessy <thepunishar1@gmail.com>
2020-02-02 21:31:51 +0200

    updating test folder

--- __test__/APIs.test.js
+++ __test__/APIs.test.js
@@ -1,24 +1,28 @@
 'use strict';
 
-// const {server} = require('../lib/server.js');
+const {server} = require('../lib/server.js');
 // for mock testing ...
-// const supergoose = require('@code-fellows/supergoose');
-// const mockRequest = supergoose(server) ;
+const supergoose = require('@code-fellows/supergoose');
+const mockRequest = supergoose(server) ;
 
 
 // ready to test the routes 
 describe('Test' , () => {
 
-  it('' , () => {
-
+  it('404 works' , ()=> {
+    return mockRequest.get('/nothing')
+      .then(data => {
+        expect(data.status).toBe(404);
+      });
   });
-  it('' , () => {
+  
+  // it('' , () => {
         
-  });
+  // });
 
-  it('' , () => {
+  // it('' , () => {
         
-  });
+  // });
 
 
 });
